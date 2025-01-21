ad707dcf73f90a0e7b37e250d62d12b5f58f507a
NicolasGrimm <hello@nicolasgrimm.ch>
2025-01-21 23:44:51 +0500

    journey

---
+++ 800/journey.cpp
@@ -0,0 +1,35 @@
+// Problem Link : https://codeforces.com/contest/2051/problem/B
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  int t = 0;
+  cin>> t;
+  
+  int n,a,b,c;
+  
+  while(t--){
+  	cin>>n>>a>>b>>c;
+  	int sum = a+b+c;
+  	 
+  	 int d = n / sum*3;
+  	 if(n%sum==0){
+  	 	cout<<d<<endl;			
+  	 	
+  	 }
+  	 else if (n % sum <= a){
+  	 	cout<<d+1<<endl;
+  	 }
+  	 else if(n % sum <= a + b){
+  	 	cout<<d+2<<endl;
+  	 }
+  	 else{
+  	 	cout<<d+3<<endl;
+  	 }
+  	 
+  	
+  }
+  return 0;
+}
